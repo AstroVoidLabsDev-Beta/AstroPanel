c557c44b96d9695f70543228592047354434678c
AstroVoidLabsDev-Beta <yashff0009@gmail.com>
2025-08-30 10:30:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// AstroPanel main server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`AstroPanel running at http://localhost:${PORT}`);
+});
